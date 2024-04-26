1a9f57f7551465118b05bd9d58ce39a07e3ef3ea
csoko99 <laczkolali77@gmail.com>
2024-04-26 21:39:44 +0200

    under maintance

--- disk_info.sh
+++ disk_info.sh
@@ -1,17 +1,18 @@
 get_disk_info() {
-    df -h | awk '{print "{\"filesystem\": \"" $1 "\", \"size\": \"" $2 "\", \"used\": \"" $3 "\", \"available\": \"" $4 "\", \"use_percentage\": \"" $5 "\", \"mounted_on\": \"" $6 "\"},"}'
+    df -h | awk '{
+        print "{ \"filesystem\": \"" $1 "\", \"size\": \"" $2 "\", \"used\": \"" $3 "\", \"available\": \"" $4 "\", \"use_percentage\": \"" $5 "\", \"mounted_on\": \"" $6 "\" }"
+    }'
 }
 
 send_data() {
     local url="$1"
     local data="$2"
-    curl -X POST -H "Content-Type: application/json" -d "[$data]" "$url"
+    curl -X POST -H "Content-Type: application/json" -d "$data" "$url"
 }
 
-url="http://example.com/post"
+url="http://localhost:3000/receive-data"
 
 disk_info=$(get_disk_info)
-
-echo "$disk_info"
-
+echo "$disk_info" > data.json  # Adatok mentése JSON fájlba
 send_data "$url" "$disk_info"
+
--- server.js
+++ server.js
@@ -1,26 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 
 const app = express();
-
 app.use(cors());
+app.use(express.json());
 
+// Adatok fogadása és mentése a JSON fájlba
 app.post('/receive-data', (req, res) => {
-  let data = '';
-
-  req.on('data', chunk => {
-    data += chunk.toString();
+  const data = req.body;
+  fs.writeFile('data.json', JSON.stringify(data), (err) => {
+      if (err) {
+          console.error('Hiba történt az adatok mentésekor:', err);
+          res.status(500).send('Hiba történt az adatok mentésekor');
+      } else {
+          console.log('Adatok sikeresen mentve');
+          res.status(200).send('Adatok sikeresen mentve');
+      }
   });
+});
 
-  req.on('end', () => {
-    console.log('Fogadott adatok:');
-    console.log(data);
 
-    res.json({ diskInfo: data });
-  });
+// Adatok visszaolvasása a JSON fájlból
+app.get('/get-data', (req, res) => {
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Hiba történt az adatok olvasásakor:', err);
+            res.status(500).send('Hiba történt az adatok olvasásakor');
+        } else {
+            const jsonData = JSON.parse(data);
+            res.status(200).json(jsonData);
+        }
+    });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`A szerver fut a http://localhost:${PORT} címen`);
+    console.log(`A szerver fut a http://localhost:${PORT} címen`);
 });
